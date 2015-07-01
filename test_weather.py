7fb188a7d4bd0b7827b7f430473c392f1745961c
wllsln <wllsln@users.noreply.github.com>
2015-07-01 01:24:18 -0700

    Added test for invalid locations, checking for message. Added test for valid location, checking for number of responses. Declare locations to test at the top of the file.

--- test_weather.py
+++ test_weather.py
@@ -3,6 +3,18 @@
 import pytest
 import unirest
 
+valid_locations = [
+    ("Los Angeles"),
+    ("San Francisco, CA"),
+    ("北京")
+]
+
+invalid_locations = [
+    ("Invalid location"),
+    ("123141...!"),
+    ("Los Angeles, Oregon")
+]
+
 class TestWeather:
     '''
     Tests the weather app.
@@ -33,19 +45,41 @@ class TestWeather:
                                params=params)
         return response
 
-    @pytest.mark.parametrize("location,code", [
-        ("Los Angeles", 200),
-        ("San Francisco, CA", 200),
-        ("Invalid location", 200),
-        ("123141...!", 200),
-        ("北京", 200)
-    ])
-    def test_location(self, location, code):
+    @pytest.mark.parametrize("location",
+                             valid_locations+invalid_locations)
+    def test_location_code(self, location):
         '''
         Test that the endpoint is functioning.
         We should get code 200 regardless of if the location is valid or
         not, just depending on if the webservice was reachable.
         '''
+        code = 200  # expected result
         response = self.request_with_location(location)
         assert response.code == code
+
+    @pytest.mark.parametrize("location", invalid_locations)
+    def test_invalid(self, location):
+        '''
+        Test that the endpoint properly reports invalid locations.
+        The response payload will have JSON response with message of
+        "Location not found".
+        '''
+        # expected result
+        code = 1
+        message = "Location not Found"
+
+        response = self.request_with_location(location)
+        json_response = response.body[0]
+        assert json_response['code'] == code
+        assert json_response['message'] == message
+
+    @pytest.mark.parametrize("location", valid_locations)
+    def test_valid_dates(self, location):
+        '''
+        Test that the endpoint properly returns proper payload dates.
+        We should have seven entries for seven dates
+        '''
+        response = self.request_with_location(location)
+        json_response = response.body
+        assert len(json_response) == 7
 
